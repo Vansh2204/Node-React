7cf0499912d992bc1f0d92e4c81bba12e45b7697
Vansh Hathi <110833474+Vansh2204@users.noreply.github.com>
2024-03-11 23:01:00 +0530

    Updated EmployeeAdd.js

--- src/components/EmployeeAdd.js
+++ src/components/EmployeeAdd.js
@@ -28,21 +28,21 @@ export function EmployeeAdd() {
 
     return (
         <>
-            <div class="container border border-dark">
+            <div class="container border border-dark mt-3" style={{ backgroundColor: "black" }}>
                 <div class="mb-3">
-                    <label for="exampleFormControlInput1" class="form-label" style={{ color: "black" }}><span class="badge text-bg-secondary">Application Name</span></label>
+                    <label for="exampleFormControlInput1" class="form-label" style={{ color: "black" }}><span class="badge text-bg-secondary">Application Name <span class="text" style={{ color: "red" }}>*</span></span></label>
                     <input type="text" value={employee.ApplicationName} class="form-control " id="exampleFormControlInput1" placeholder="Name" onChange={(e) => { setemployee({ ...employee, ApplicationName: e.target.value }) }} />
                 </div>
                 <div class="mb-3">
-                    <label for="exampleFormControlInput1" class="form-label" style={{ color: "black" }}><span class="badge text-bg-secondary">Position Applied For</span></label>
+                    <label for="exampleFormControlInput1" class="form-label" style={{ color: "black" }}><span class="badge text-bg-secondary">Position Applied For <span class="text" style={{ color: "red" }}>*</span></span></label>
                     <input type="text" value={employee.PositionAppliedFor} class="form-control " id="exampleFormControlInput1" placeholder="Position" onChange={(e) => { setemployee({ ...employee, PositionAppliedFor: e.target.value }) }} />
                 </div>
                 <div class="mb-3">
-                    <label for="exampleFormControlInput1" class="form-label" style={{ color: "black" }}><span class="badge text-bg-secondary">Application Date</span></label>
+                    <label for="exampleFormControlInput1" class="form-label" style={{ color: "black" }}><span class="badge text-bg-secondary">Application Date <span class="text" style={{ color: "red" }}>*</span></span></label>
                     <input type="text" value={employee.ApplicationDate} class="form-control " id="exampleFormControlInput1" placeholder="YY-MM-DD" onChange={(e) => { setemployee({ ...employee, ApplicationDate: e.target.value }) }} />
                 </div>
                 <div class="mb-3">
-                    <label for="exampleFormControlInput1" class="form-label " style={{ color: "black" }}><span class="badge text-bg-secondary">Status </span></label>
+                    <label for="exampleFormControlInput1" class="form-label " style={{ color: "black" }}><span class="badge text-bg-secondary">Status <span class="text" style={{ color: "red" }}>*</span> </span></label>
                     <input type="text" value={employee.Status} class="form-control " id="exampleFormControlInput1" placeholder="Status" onChange={(e) => { setemployee({ ...employee, Status: e.target.value }) }} />
                 </div>
             </div>
@@ -52,7 +52,7 @@ export function EmployeeAdd() {
             <div class="container" style={{ alignItems: "center" }}>
                 <div class="row justify-content-center">
                     <div class="p-1">
-                        <a href="#" class="btn btn-primary" onClick={() => {
+                        <a href="#" class="btn btn-light mt-3" onClick={() => {
                             if (params.id > 0) {
                                 fetch(
                                     "http://localhost:5000/employees" +
